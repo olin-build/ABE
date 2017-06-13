8d3d20891422a9e7c55a60e3c768870ba55c5b28
Evan Lloyd New-Schmidt <lloydns@outlook.com>
2017-06-13 13:32:40 -0400

    Change flask port

--- app.py
+++ app.py
@@ -101,5 +101,5 @@ def calendarDelete():
 if __name__ == '__main__':
    app.debug = True  # updates the page as the code is saved
    HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
-   PORT = int(os.environ.get('PORT', 443))
+   PORT = int(os.environ.get('PORT', 3000))
    app.run(host='0.0.0.0', port=PORT)
