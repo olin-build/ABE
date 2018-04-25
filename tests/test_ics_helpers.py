10caa535eaaa9a46c656a7efaa1b1b342b3155b0
Oliver Steele <steele@osteele.com>
2018-04-25 12:13:25 -0400

    Better comments re ics tests

--- tests/test_ics_helpers.py
+++ tests/test_ics_helpers.py
@@ -55,6 +55,7 @@ testEvents = dict(
 
 class IcsHelpersTestCase(abe_unittest.TestCase):
 
+    # TODO: DRY w/ method in RecurrenceTestCase
     def get_test_event(self, key):
         db = self.db
         assert key in key
@@ -70,11 +71,10 @@ class IcsHelpersTestCase(abe_unittest.TestCase):
             self.assertEqual(ics_event.get('description'), icalendar.vText("Event description"))
             self.assertEqual(ics_event.get('location'), icalendar.vText("Room of Requirement"))
             # TODO: test dtstart and dtend
-            # self.assertEqual(ics_event.get('dtstart'), None)
-            # self.assertEqual(ics_event.get('dtend'), None)
-
+            # self.assertEqual(ics_event.get('dtstart'), …)
+            # self.assertEqual(ics_event.get('dtend'), …)
             self.assertEqual(ics_event.get('RECURRENCE-ID'), None)
-            # self.assertEqual(ics_event.get('rrule'), None)
+            self.assertEqual(ics_event.get('rrule'), None)
 
         with self.subTest('all-day event'):
             event = self.get_test_event('all_day')
@@ -83,37 +83,38 @@ class IcsHelpersTestCase(abe_unittest.TestCase):
             self.assertEqual(ics_event.get('dtstart'), None)
             self.assertEqual(ics_event.get('dtend'), None)
 
-        with self.subTest('recurring event'):
+        with self.subTest('recurring events'):
             self.skipTest('unimplemented test')
             # TODO: test that id is from event['sid']
             # TODO: test RECURRENCE_ID from event['rec_id']
+            # TODO: test byday, bymonthday, byyearday in RRULE
 
     @skip('Unimplemented test')
     def test_create_ics_recurrence(self):
-        # TODO:
-        ics_helpers.create_ics_recurrence(new_event, recurrence)
+        pass
+        # TODO: ics_helpers.create_ics_recurrence(new_event, recurrence)
 
     @skip('Unimplemented test')
     def test_mongo_to_ics(self):
-        # TODO:
-        ics_helpers.mongo_to_ics(events)
+        pass
+        # TODO: ics_helpers.mongo_to_ics(events)
 
     @skip('Unimplemented test')
     def test_ics_to_dict(self):
-        # TODO:
-        ics_helpers.ics_to_dict(component, labels, ics_id=None)
+        pass
+        # TODO: ics_helpers.ics_to_dict(component, labels, ics_id=None)
 
     @skip('Unimplemented test')
     def test_extract_ics(self):
-        # TODO:
-        ics_helpers.extract_ics(cal, ics_url, labels=None)
+        pass
+        # TODO: ics_helpers.extract_ics(cal, ics_url, labels=None)
 
     @skip('Unimplemented test')
     def test_update_ics_to_mongo(self):
-        # TODO:
-        ics_helpers.update_ics_to_mongo(component, labels)
+        pass
+        # TODO: ics_helpers.update_ics_to_mongo(component, labels)
 
     @skip('Unimplemented test')
     def test_update_ics_feed(self):
-        # TODO:
-        ics_helpers.update_ics_feed()
+        pass
+        # TODO: ics_helpers.update_ics_feed()
