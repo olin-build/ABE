272af43d8c903920b85767d8d423c4c21daf4b67
jwb <jared.briskman@students.olin.edu>
2018-05-04 14:47:00 -0400

    Adds working cookie auth

--- abe/auth/__init__.py
+++ abe/auth/__init__.py
@@ -17,10 +17,10 @@ from netaddr import IPNetwork, IPSet
 INTRANET_IPS = (IPSet([IPNetwork(s) for s in os.environ.get('INTRANET_IPS', '').split(',')])
                 if 'INTRANET_IPS' in os.environ else IPSet(['0.0.0.0/0', '0000:000::/0']))
 
-shared_secret = os.environ["SHARED_SECRET"] # Should fail if not set
+shared_secret = os.environ.get("SHARED_SECRET", None)
 
 
-def after_this_request(f): # For setting cookie
+def after_this_request(f):  # For setting cookie
     if not hasattr(g, 'after_request_callbacks'):
         g.after_request_callbacks = []
     g.after_request_callbacks.append(f)
@@ -35,11 +35,11 @@ def edit_auth_required(f):
             'X-Forwarded-For', request.remote_addr).split(',')[-1]
         if client_ip in INTRANET_IPS:
             @after_this_request
-            def remember_language(response):
+            def remember_computer(response):
                 response.set_cookie('app_secret', shared_secret)
         else:
             with request.cookies.get('app_secret') as app_secret:
-                if app_secret != shared_secret:
+                if (not shared_secret) or (app_secret != shared_secret):
                     abort(401)
         return f(*args, **kwargs)
     return wrapped
