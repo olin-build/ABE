7ac3989cdd11fe26b87289016050c821d238a43c
Oliver Steele <steele@osteele.com>
2018-05-18 19:22:07 -0400

    Switch to pytest
    This works around some issues involving module import order, as well as being a nicer tool

--- tests/context.py
+++ tests/context.py
@@ -3,16 +3,14 @@ Referenced from The Hitchhiker's Guide to Python.
 http://docs.python-guide.org/en/latest/writing/structure/#test-suite
 """
 import os
-import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 MONGODB_TEST_DB_NAME = "abe-unittest"
+os.environ['ADMIN_EMAILS'] = 'abe-admin@olin.edu'
 os.environ["DB_NAME"] = MONGODB_TEST_DB_NAME
 os.environ["INTRANET_CDIRS"] = "127.0.0.1/24"
 os.environ["MONGODB_URI"] = ""
 os.environ['SLACK_OAUTH_CLIENT_ID'] = 'slack-oauth-client-id'
-os.environ['ADMIN_EMAILS'] = 'abe-admin@olin.edu'
 
 os.environ['ABE_EMAIL_USERNAME'] = 'email-test-user'
 os.environ['ABE_EMAIL_PASSWORD'] = 'email-test-password'
